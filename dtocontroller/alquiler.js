a17fce57fdc62e69ada22c0346cf4dc4474ee325
VickyMontanezCampus <vmontanez707@gmail.com>
2023-08-25 07:09:05 -0500

    feat: :sparkles: Add a Dto to the alquiler table
    Add a new Dto with their respective validations of the alquiler table

--- dtocontroller/alquiler.js
+++ dtocontroller/alquiler.js
@@ -8,70 +8,60 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 import { Expose } from 'class-transformer';
-import { IsDefined, IsString, Matches } from 'class-validator';
+import { IsDefined, IsString, Matches, IsInt } from 'class-validator';
 export class Alquiler {
     constructor(data) {
         Object.assign(this, data);
-        this.ID_Alquiler = 0;
-        this.ID_Cliente_id = 0;
-        this.ID_Automovil_id = 0;
-        this.Fecha_Inicio = "1991-01-01";
-        this.Fecha_Fin = "1991-01-01";
-        this.Costo_Total = 0;
-        this.Estado = "";
+        this.id_alquiler = 0;
+        this.id_cliente_id = 0;
+        this.id_automovil_id = 0;
+        this.fecha_inicio = new Date();
+        this.fecha_fin = new Date();
+        this.costo_total = 0;
+        this.estado = '';
     }
 }
 __decorate([
-    Expose({ name: 'rent' })
-    // @IsNumber({}, { message: () => { throw { status: 422, message: `El cedula_usuario no cumple con el formato, debe ser un numero`}}})
-    ,
-    IsDefined({ message: () => { throw { status: 422, message: `El parametro rent es obligatorio` }; } }),
+    Expose({ name: 'alquiler' }),
+    IsInt({ message: 'The parameter alquiler must be a number' }),
+    IsDefined({ message: 'The parameter alquiler is required' }),
     __metadata("design:type", Number)
-], Alquiler.prototype, "ID_Alquiler", void 0);
+], Alquiler.prototype, "id_alquiler", void 0);
 __decorate([
-    Expose({ name: 'client' })
-    // @IsNumber({}, { message: () => { throw { status: 422, message: `El cedula_usuario no cumple con el formato, debe ser un numero`}}})
-    ,
-    IsDefined({ message: () => { throw { status: 422, message: `El parametro client es obligatorio` }; } }),
+    Expose({ name: 'cliente' }),
+    IsInt({ message: 'The parameter cliente must be a number' }),
+    IsDefined({ message: 'The parameter cliente is required' }),
     __metadata("design:type", Number)
-], Alquiler.prototype, "ID_Cliente_id", void 0);
+], Alquiler.prototype, "id_cliente_id", void 0);
 __decorate([
-    Expose({ name: 'car' })
-    // @IsNumber({}, { message: () => { throw { status: 422, message: `El cedula_usuario no cumple con el formato, debe ser un numero`}}})
-    ,
-    IsDefined({ message: () => { throw { status: 422, message: `El parametro car es obligatorio` }; } }),
+    Expose({ name: 'automovil' }),
+    IsInt({ message: 'The parameter automovil must be a number' }),
+    IsDefined({ message: 'The parameter automovil is required' }),
     __metadata("design:type", Number)
-], Alquiler.prototype, "ID_Automovil_id", void 0);
+], Alquiler.prototype, "id_automovil_id", void 0);
 __decorate([
-    Expose({ name: 'initDate' })
-    // @IsNumber({}, { message: () => { throw { status: 422, message: `El cedula_usuario no cumple con el formato, debe ser un numero`}}})
-    ,
-    IsString({ message: 'El parametro initDate debe ser un string' }),
-    IsDefined({ message: () => { throw { status: 422, message: `El parametro initDate es obligatorio` }; } }),
-    Matches(/^\d{4}-\d{2}-\d{2$}/, { message: 'Error' }),
+    Expose({ name: 'fechaInicio' }),
+    IsString({ message: 'The parameter fechaInicio must be a date' }),
+    IsDefined({ message: 'The parameter fechaInicio is required' }),
+    Matches(/^\d{4}-\d{2}-\d{2}$/, { message: 'Invalid format for fechaInicio' }),
+    __metadata("design:type", Date)
+], Alquiler.prototype, "fecha_inicio", void 0);
+__decorate([
+    Expose({ name: 'fechaFin' }),
+    IsString({ message: 'The parameter fechaFin must be a date' }),
+    IsDefined({ message: 'The parameter fechaFin is required' }),
+    Matches(/^\d{4}-\d{2}-\d{2}$/, { message: 'Invalid format for fechaFin' }),
+    __metadata("design:type", Date)
+], Alquiler.prototype, "fecha_fin", void 0);
+__decorate([
+    Expose({ name: 'costoTotal' }),
+    IsInt({ message: 'The parameter costoTotal must be a number' }),
+    IsDefined({ message: 'The parameter costoTotal is required' }),
+    __metadata("design:type", Number)
+], Alquiler.prototype, "costo_total", void 0);
+__decorate([
+    Expose({ name: 'estado' }),
+    IsString({ message: 'The parameter estado must be a string' }),
+    IsDefined({ message: 'The parameter estado is required' }),
     __metadata("design:type", String)
-], Alquiler.prototype, "Fecha_Inicio", void 0);
-__decorate([
-    Expose({ name: 'endDate' })
-    // @IsNumber({}, { message: () => { throw { status: 422, message: `El cedula_usuario no cumple con el formato, debe ser un numero`}}})
-    ,
-    IsString({ message: 'El parametro endDate debe ser un string' }),
-    IsDefined({ message: () => { throw { status: 422, message: `El parametro endDate es obligatorio` }; } }),
-    Matches(/^\d{4}-\d{2}-\d{2$}/, { message: 'Error' }),
-    __metadata("design:type", String)
-], Alquiler.prototype, "Fecha_Fin", void 0);
-__decorate([
-    Expose({ name: 'totalCost' })
-    // @IsNumber({}, { message: () => { throw { status: 422, message: `El cedula_usuario no cumple con el formato, debe ser un numero`}}})
-    ,
-    IsDefined({ message: () => { throw { status: 422, message: `El parametro totalCost es obligatorio` }; } }),
-    __metadata("design:type", Number)
-], Alquiler.prototype, "Costo_Total", void 0);
-__decorate([
-    Expose({ name: 'estate' })
-    // @IsNumber({}, { message: () => { throw { status: 422, message: `El cedula_usuario no cumple con el formato, debe ser un numero`}}})
-    ,
-    IsDefined({ message: () => { throw { status: 422, message: `El parametro estate es obligatorio` }; } }),
-    __metadata("design:type", String)
-], Alquiler.prototype, "Estado", void 0);
-;
+], Alquiler.prototype, "estado", void 0);
